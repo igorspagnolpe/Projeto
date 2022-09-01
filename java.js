990a2d8ea9fb9c237777cb1d932c802983bfa3ef
igorspagnolpe <102969338+igorspagnolpe@users.noreply.github.com>
2022-09-01 13:35:34 -0300

    Update java.js
    coloca isso

--- java.js
+++ java.js
@@ -1,10 +1,23 @@
+var inputs = document.getElementById('comentario');
+for(var i = 0; i < 1; i++){
+    inputs.addEventListener('focus', function(){
+    var place = this.getAttribute('placeholder');
+    this.setAttribute('placeholder', '');
+    var blur = function(){
+      this.setAttribute('placeholder', place);
+    }
+    this.addEventListener('blur', blur);
+  });
+}
+
 function mensagem(){
-    const texto = document.querySelector('#comentario')
-    if(comentario.value == ""){
+    // const texto = document.querySelector('#comentario')
+    var texto = document.getElementById('comentario')   
+    if(texto.value == ""){
         alert("Preencha todos os formulários")
     }
 
     else{
         alert("Mensagem enviada! Obrigado.")
-    }
+    }    
 }
