d05513ecb4882db0ea3a704acdcb9372c81c4d69
isaacbribeiro <110836082+isaacbribeiro@users.noreply.github.com>
2022-08-31 22:43:58 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+function mensagem(){
+    const texto = document.querySelector('#comentario')
+    if(comentario.value == ""){
+        alert("Preencha todos os formulários")
+    }
+
+    else{
+        alert("Mensagem enviada! Obrigado.")
+    }
+}
